7b630eef1795860c96195bd60027627701b903d5
Konstantin BIFERT <konstantin.bifert@gmail.com>
2023-11-06 16:01:33 +0100

    done

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,11 @@
     <NuxtLink to="/calculators">go to calculators</NuxtLink>
     <br />
     <br />
-    <nuxt-link v-slot="{ route, navigate }" to="https://nuxt2-nuxtlink-repro.netlify.app/calculators" custom>
-      <a @click="navigate">Go to {{ route.fullPath }}</a>
+    <nuxt-link v-slot="{ route, navigate }" to="/about" custom>
+      <a @click="navigate">Go to {{ route.fullPath }} (non-working)</a>
+      <a :href="'https://nuxt2-nuxtlink-repro.netlify.app' + '/about'" @click="navigate">
+        Go to {{ route.fullPath }}
+      </a>
     </nuxt-link>
   </div>
 </template>
